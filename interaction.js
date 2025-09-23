ca4c73263d04611a1106ebb749f95d7c90f64232
EwanP56 <ewanpenven@gmail.com>
2025-09-23 15:56:31 +0200

    Q1 à Q5 environ

--- interaction.js
+++ interaction.js
@@ -3,10 +3,43 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
+  this.canvas = canvas;
+  this.interactor = interactor;
+  this.dragging = true;
+  this.startX = 0;
+  this.startY = 0;
+  this.lastX = 0;
+  this.lastY = 0;
 
 	// Developper les 3 fonctions gérant les événements
+  this.clicSouris = function(evt) {
+    var mousePos = getMousePosition(canvas, evt);
+    this.startX = mousePos.x;
+    this.startY = mousePos.y;
+    this.lastX = mousePos.x;
+    this.lastY = mousePos.y;
+  }.bind(this);
+
+  this.deplacementSouris = function(evt) {
+    if (this.dragging) {
+      var mousePos = getMousePosition(canvas, evt);
+      this.lastX = mousePos.x;
+      this.lastY = mousePos.y;
+      this.dragging = true;
+    }
+  }.bind(this);
+
+  this.relacherSouris = function(evt) {
+    var mousePos = getMousePosition(canvas, evt);
+    this.lastX = mousePos.x;
+    this.lastY = mousePos.y;
+    this.dragging = false;
+  }.bind(this);
 
 	// Associer les fonctions précédentes aux évènements du canvas.
+  canvas.addEventListener('mousedown', this.clicSouris, false);
+  canvas.addEventListener('mousemove', this.deplacementSouris, false);
+  canvas.addEventListener('mouseup', this.relacherSouris, false);
 };
 
 
--- model.js
+++ model.js
@@ -1,3 +1,34 @@
-
-// Implémenter ici les 4 classes du modèle.
+// Implémenter ici les 4 functiones du modèle.
 // N'oubliez pas l'héritage !
+
+//Drwaning se composant de Formes
+function Drawing() {
+    this.formesArray = new Map();
+}
+
+function Formes(couleur, epaisseur, ) {
+    this.couleur = couleur;
+    this.epaisseur = epaisseur;
+    //Utilisation de Array
+    this.listFormes = new Array();
+}
+
+function Rectangle(coorHautGaucheX, coorHautGaucheY, largeur, hauteur, epaisseur, couleur) {
+    Formes.call(this, couleur, epaisseur);
+    this.coorHautGaucheX = coorHautGaucheX;
+    this.coorHautGaucheY = coorHautGaucheY;
+    this.largeur = largeur;
+    this.hauteur = hauteur;
+}
+
+function Line(coorPointA, coorPointB, epaisseur, couleur) {
+    Formes.call(this, couleur, epaisseur);
+    this.coors = [coorPointA, coorPointB];
+}
+
+/*
+function Femme(poids, taille) {
+  Humain.call(this, poids, taille);
+};
+Femme.prototype = new Humain();
+*/
